f8173088e3749b5c6bef7d7c6f3c729bbd919d96
Jonatas <85264333+Jonatas00@users.noreply.github.com>
2023-05-04 10:26:51 -0300

    Update gerais.py

--- comandos/gerais.py
+++ comandos/gerais.py
@@ -5,6 +5,6 @@ class Gerais(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name = 'hello', description = 'Comando que retorna Hello')
+    @commands.slash_command(name='hello', description='Comando que retorna Hello')
     async def hello(self, interaction: discord.Interaction):
         await interaction.response.send_message("Olá, meu nome é Astêmio!")
